80e95b2dd5c151f50493fb45eae514868bcb4018
lucas barros <lucasbarrosjl04@gmail.com>
2026-04-20 23:38:01 -0300

    feat: adiciona sistema de tabuada com repetição e método

--- src/com/lucas/fundamentos/estruturarepeticao/RepeticaoWhile.java
+++ src/com/lucas/fundamentos/estruturarepeticao/RepeticaoWhile.java
@@ -1,15 +1,30 @@
 package com.lucas.fundamentos.estruturarepeticao;
 
-
 import java.util.Scanner;
 
 public class RepeticaoWhile {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int contador = 1;
 
-        System.out.print("Você deseja ver a tabuada de qual número: ");
-        int numero = input.nextInt();
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String res = "Sim";
+
+        while (res.equalsIgnoreCase("SIM")) {
+            System.out.print("Você deseja ver a tabuada de qual número: ");
+            int numero = input.nextInt();
+
+            mostrarTabuada(numero);
+
+            System.out.println("Você deseja ver outra tabuada? [Sim/Nao]");
+            res = input.next();
+
+        }
+
+        input.close();
+    }
+
+    public static void mostrarTabuada(int numero) {
+        int contador = 1;
 
         while (contador <= 10) { // Estrutura de repetição while.
             int resultado = (numero * contador);
@@ -18,13 +33,9 @@ public class RepeticaoWhile {
 
             System.out.println(numero + " x " + contador + " = " + resultado + tipo); // imprime na tela
 
-
-
             contador++; // adiciona +1 no contador
 
         }
-
-     input.close();
-
     }
+
 }
